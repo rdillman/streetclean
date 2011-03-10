27eecb086aaa2375008f0286db76272e43893448
Richard Dillman <dillman85@gmail.com>
2011-03-10 11:36:35 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Streetclean::Application.config.secret_token = '4673b72dd2e192006423adf28f69749570849948769328149cedae744977799b0df6575fbbb217a62429d7ce52639d3e5c3ac890fb982a64a3565843fd332c89'
