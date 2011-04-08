206b6d65d25e324c385406fed9b4b09168bcdd89
Richard Dillman <dillman85@gmail.com>
2011-04-08 12:31:53 -0700

    added the google analytics for ticketcricket.heroku.com

--- db/schema.rb
+++ db/schema.rb
@@ -58,4 +58,9 @@ ActiveRecord::Schema.define(:version => 20110405023333) do
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
+  create_table "vehicles", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
 end
