665bfa0edafb6a98340a98ba4d1190a5a24b3ef1
Peter O'Braidan <pspetroff@gmail.com>
2011-04-14 13:08:55 -0700

    Added some comments!

--- app/models/ct.rb
+++ app/models/ct.rb
@@ -1,6 +1,7 @@
 class Ct < ActiveRecord::Base
   has_many :blocks
 
+  #Determines whether a given date is a SFDPW Holiday
   def is_holiday?(time)
     holidays = ["January 01","January 17","February 21","May 30","July 04","September 05","October 10","November 11","November 24","November 25","December 25"]
     if holidays.find{|x| x == time.strftime("%B %d")}
@@ -9,6 +10,7 @@ class Ct < ActiveRecord::Base
     return FALSE
   end
 
+  #Sets warning if the street is currently being cleaned
   def self.set_warning(start, stop,now)
     if stop > now && start < now
       start = start + 0.000001
@@ -16,6 +18,7 @@ class Ct < ActiveRecord::Base
     return start,stop
   end
     
+    #Formats the next cleantime start and stop for public consumption
   def prepare_nt(nt,stop,now)
     stop = nt.strftime("%D")<<" "<<stop
     next_time = Array.new
@@ -25,7 +28,7 @@ class Ct < ActiveRecord::Base
   end
     
     
-  
+  #Used to turn  the "Hol" days in a CT into actual dates (e.g. "January 1" for New Years)
   def self.get_next_holiday(now)
     smallest = 2.year
     next_hol = ""
@@ -41,22 +44,27 @@ class Ct < ActiveRecord::Base
     return next_hol
   end
   
+  #Returns true is a CT is cleaned on a holiday
   def holiday_clean?
     self.boolyuns.at(0) == 'T'
   end
   
+  #Returns true if a CT is not repeated every week of the month 
   def non_weekly?
     self.boolyuns.from(1) != "TTTTT"
   end
   
+  #Returns Chron parse of the start time
   def start_time
     Chronic.parse(self.wday+" at "+self.start)
   end
   
+  #Returns Chron parse of a stop time
   def stop_time
     Chronic.parse(self.wday+" at "+self.stop)
   end
   
+  #Figures out when the next non-weekly sweep for a CT is 
   def non_weekly_times(now)
     stop = self.stop
     this_month = 0
@@ -66,6 +74,8 @@ class Ct < ActiveRecord::Base
       month = Chronic.parse(month<<" months from now")
       month = month.strftime("%B")
   
+      #If a non-weekly is swept on a day of the week - that next time is returned
+      #Since this function moves in order chronologically it returns the first clean time that is after now
       if self.boolyuns[1] == 84
         next_time = Chronic.parse("1st "<<self.wday<<" in "<<month)
         next_time = self.chronify_other_weeks(next_time)
@@ -106,6 +116,7 @@ class Ct < ActiveRecord::Base
     end
   end
   
+  #Helper Function for
   def chronify_other_weeks(next_time)
     next_time_str = next_time.strftime("%D")
     next_time = Chronic.parse(next_time_str<<" "<<self.start)
@@ -117,7 +128,7 @@ class Ct < ActiveRecord::Base
     end
   end
       
-  #This function returns the start and stop times of the next clean time
+  #This function returns the start and stop times of the next clean time as time object
   def next_time
     now = Time.now
     start = self.start_time
--- app/models/street.rb
+++ app/models/street.rb
@@ -13,9 +13,14 @@ class Street < ActiveRecord::Base
   
   #This function returns the next clean time for a given address on the street
   def next_clean_time(addr)  
-    side = self.get_side(addr)  
+    #Figure out side of the street
+    side = self.get_side(addr) 
+    
+    #Find all blocks that match Street Address 
     all_blocks = self.blocks
     blocks = all_blocks.where("side = ? AND bottom <= ? AND top >= ?", side, addr, addr)
+    
+    #Compute the next cleaning times for the block and pick the smallest(i.e. the soonest) time
     cts = Array.new
     blocks.each{|x|cts << x.ct.next_time}
     best = cts.min
