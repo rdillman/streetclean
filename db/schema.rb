da7e1856c1cd390fcef940cee63bffce74d07b72
Peter O'Braidan <pspetroff@gmail.com>
2011-05-06 13:05:50 -0700

    added database file?

--- db/schema.rb
+++ db/schema.rb
@@ -87,11 +87,11 @@ ActiveRecord::Schema.define(:version => 20110426183814) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
     t.string   "username"
     t.string   "phone_number"
     t.string   "carrier"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
